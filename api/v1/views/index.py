0a6b731615a5772174d913823ab3c8b402426d2d
Nicholas Otieno <otienonicholas02@gmail.com>
2023-10-03 00:01:08 +0300

    some stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -10,3 +10,19 @@ from models import storage
 def status_check():
     """Returns OK status code"""
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats')
+def get_stats():
+    '''Gets the number of objects for each type.
+    '''
+    objects = {
+        'amenities': Amenity,
+        'cities': City,
+        'places': Place,
+        'reviews': Review,
+        'states': State,
+        'users': User
+    }
+    for key, value in objects.items():
+        objects[key] = storage.count(value)
+    return jsonify(objects)
