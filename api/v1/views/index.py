acc04048eac53bc74bd5288f90515bbbe52c80c9
MesfinGithinji <meshgithinji@gmail.com>
2023-10-03 01:28:40 +0300

    Status of your API

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,26 +2,35 @@
 """Initial routes"""
 
 from flask import jsonify
+
 from api.v1.views import app_views
 from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route('/status')
-def status_check():
-    """Returns OK status code"""
-    return jsonify({"status": "OK"})
+def get_status():
+    """Returns a status check"""
+    return jsonify(status='OK')
 
 
 @app_views.route('/stats')
 def get_stats():
-    '''Gets the number of objects for each type.
-    '''
+    """Gets the number of objects for each type.
+    """
     objects = {
-        "amenities": storage.count('Amenity'),
-        "cities": storage.count('City'),
-        "places": storage.count('Place'),
-        "reviews": storage.count('Review'),
-        "states": storage.count('State'),
-        "users": storage.count('User'),
+        'amenities': Amenity,
+        'cities': City,
+        'places': Place,
+        'reviews': Review,
+        'states': State,
+        'users': User
     }
+    for key, value in objects.items():
+        objects[key] = storage.count(value)
     return jsonify(objects)
